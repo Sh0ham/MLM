dff9d77c0e26bf81e8401b620c94dc79002e0320
Sh0ham <Sh0ham@users.noreply.github.com>
2020-02-09 12:35:59 +0200

    Add files via upload

---
+++ Lab01/prog1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>   /*for printf*/      
+#include <conio.h>   /*for getch*/                      
+
+int main()
+{
+	printf("Hello World!\nWe are working on our first lab in Introduction to Computer Science Course.");/*print Hello World! and go to the
+																										//beginning of the next new line, than print the comment requested */
+	getch();                  /*input a character without printing*/
+	return 0;
+}
---
+++ Lab01/prog2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>   /*for printf*/      
+#include <conio.h>   /*for getch*/                      
+
+int main()
+{
+	printf("We are drawing a 4x5 rectangle:\n\n*****\n*   *\n*   *\n*****"); /*print tittle and than the asterisks rectangle*/
+	getch();                  /*input a character without printing*/
+	return 0;
+}
